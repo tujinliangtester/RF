8f5fea0c7f58c05dde50d3548b682d85dade592a
tujinliangtester <tujinliangtester@163.com>
2019-02-15 16:21:30 +0800

    一些简单整理

---
+++ tmp.py
@@ -0,0 +1,13 @@
+s='product_type_id=10&longitude=104.14189910888672&latitude=30.577411651611328&distance=30&orderbyfield=pointdistance&site_id=&site_name=&pageNumber=1&pagesize=10&r=0.275035247685235'
+
+l=s.split('&')
+print(l)
+
+s2='product_type_id=10&longitude=104.14189910888672&latitude=30.577411651611328&distance=30&orderbyfield=pointdistance&site_id=&site_name=&pageNumber=1&pagesize=10&r=0.5619037491133262'
+
+l=s2.split('&')
+print(l)
+
+s3='product_type_id=10&longitude=104.06708526611328&latitude=30.547513961791992&distance=30&orderbyfield=pointdistance&site_id=&site_name=&pageNumber=1&pagesize=10&r=0.13441391589473906'
+l=s3.split('&')
+print(l)
