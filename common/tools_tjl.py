c55cfdc0146cd728cb3ea340e1149d8cb355e742
qc_0dcdcfc4ed0b4f7f89500bb54c3818c2 <qc_0dcdcfc4ed0b4f7f89500bb54c3818c2@git.code.tencent.com>
2019-03-18 11:37:15 +0800

    继续处理xlsx

--- common/tools_tjl.py
+++ common/tools_tjl.py
@@ -22,7 +22,7 @@ def url_md_decoder(s):
 
 if __name__ == '__main__':
     # 注意，如果参数中有中文，则需要在替换后，手动改成中文
-    s = 'md%5Busername%5D=19903071333&md%5Bname%5D=%E6%8E%A8%E5%B9%BF%E5%91%9837&md%5Bparter_id%5D=2&md%5Broles%5D=2&md%5Bmobile%5D=19903071333&md%5Bemail%5D=&md%5Bwx_account%5D=&md%5Bpay_account%5D=&md%5Bsettlement_type%5D=1&md%5Breg_user_amount%5D=1.00'
+    s = 'ts=2019-03-13T01%3A58%3A21.555Z&t=timing&sid=jt6k1lfv1f1&px=360x720&rf=http%3A%2F%2F192.168.10.236%3A8025%2FCreditcardPingan%2FPackage&v=1&ua=Chrome&evt=pageview&wx=1&sit=openh5&t1=%E5%B9%B3%E5%AE%89%E9%93%B6%E8%A1%8C%E6%B4%BB%E5%8A%A8%E8%90%BD%E5%9C%B0%E9%A1%B5&t2=%E4%B8%AD%E5%9B%BD%E5%B9%B3%E5%AE%89-%E9%A2%86%E5%8F%96%E4%BC%98%E6%83%A0%E5%88%B8&t3=%E9%A1%B5%E9%9D%A2%E5%8A%A0%E8%BD%BD&mrt=pakdyh_activty'
     s2 = str(str2dic(s))
     s2 = url_md_decoder(s2)
     s2 = s2.replace('\'', '\"')
--- lib/tools.py
+++ lib/tools.py
@@ -1,40 +1,93 @@
 import time
-import xlrd,xlwt
-import sys,os
+import xlrd, xlwt
+import sys, os
+from openpyxl import load_workbook
+
+
 class tools(object):
+    def __init__(self):
+        self._sheet_name = 'Sheet3'
+        self._max_line = 20
+
     def gene_mobile(self):
-        s=time.strftime("%Y%m%d%H%M", time.localtime())
-        s=s[-8:]
-        s='199'+s
+        s = time.strftime("%Y%m%d%H%M", time.localtime())
+        s = s[-8:]
+        s = '199' + s
         print(s)
         return s
 
-    def read_data(self,key):
-        path=os.path.join(os.path.dirname(__file__),'../config/setting.xlsx')
-        tmp_dic={}
-        wb=xlrd.open_workbook(path)
-        ws=wb.sheet_by_name('Sheet3')
-        i=2
+    def read_data(self, key):
+        path = os.path.join(os.path.dirname(__file__), '../config/setting.xlsx')
+        tmp_dic = {}
+        wb = xlrd.open_workbook(path)
+        ws = wb.sheet_by_name(self._sheet_name)
+        i = 2
         try:
-            while(i<20):
-                tmp_key=ws.cell_value(i,1)
-                if(tmp_key is None):
+            while (i < self._max_line):
+                tmp_key = ws.cell_value(i, 1)
+                if (tmp_key is None):
                     break
-                tmp_dic[tmp_key]=ws.cell_value(i,2)
-                i+=1
+                tmp_dic[tmp_key] = ws.cell_value(i, 2)
+                i += 1
         except Exception:
             print(Exception)
 
-        return  tmp_dic[key]
-    def write_data(self,s):
+        return tmp_dic[key]
+
+    # 这个函数还没有写完，这个函数只能新增文件，不能修改已存在文件
+    def add_xlsx(self, s):
         path = os.path.join(os.path.dirname(__file__), '../config/setting.xlsx')
         tmp_dic = {}
         wb = xlwt.Workbook(path)
-        ws = wb.sheet_index('Sheet3')
+        ws = wb.sheet_index(self._sheet_name)
+
+    def edit_xlsx(self, dic):
+        path = os.path.join(os.path.dirname(__file__), '../config/setting.xlsx')
+        wb = load_workbook(path)
+        ws = wb[self._sheet_name]
+        i = 3
+        flag = False
+        '''
+        try:
+            while (i < self._max_line):
+                s = 'B' + str(i)
+                for key in dic.keys():
+                    if (ws[s].value is None):
+                        break
+                    if (ws[s].value == key):
+                        s2 = 'C' + str(i)
+                        ws[s2] = dic[key]
+                        flag = True
+                        break
+                if (flag): break
+                i += 1
+        except Exception:
+            print(Exception.mro())
+            
+            '''
+        while (i < self._max_line):
+            s = 'B' + str(i)
+            for key in dic.keys():
+                if (ws[s].value is None):
+                    break
+                if (ws[s].value == key):
+                    s2 = 'C' + str(i)
+                    ws[s2] = dic[key]
+                    flag = True
+                    break
+            if (flag): break
+            i += 1
+        if (flag == False):  # 说明xlsx中不存在这个字段
+            print(111)
+            s = 'B' + str(i)
+            ws[s] = dic.keys[0]
+            s = 'C' + str(i)
+            ws[s] = dic[dic.keys[0]]
 
 
-
-if __name__=='__main__':
-    t=tools()
-    t.gene_mobile()
+if __name__ == '__main__':
+    t = tools()
+    # t.gene_mobile()
     print(t.read_data('mobile'))
+    dic = {'mobile': 123}
+    t.edit_xlsx(dic)
